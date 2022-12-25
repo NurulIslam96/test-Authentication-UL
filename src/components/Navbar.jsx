da671c69267a12c0f23e9a4bcb6b049f129dc859
NurulIslam96 <nurul.islam96@aol.com>
2022-12-26 04:36:18 +0600

    fixed bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,36 +4,41 @@ import { AuthContext } from "../context/AuthProvider";
 
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
-  const handleLogout = () =>{
-    logout()
-  }
+  const handleLogout = () => {
+    logout();
+  };
   return (
     <div className="navbar bg-blue-50 flex justify-between">
       <div>
-        <Link to={'/'} className="btn btn-outline normal-case text-xl">Test-UnioLabs</Link>
-      </div>
-      {
-        !user?.email ? <div className="flex gap-2">
-        <Link to={"/register"}>
-          <button className="btn">Register</button>
-        </Link>
-        <Link to={"/login"}>
-          <button className="btn">Login</button>
+        <Link to={"/"} className="btn btn-outline normal-case text-xl">
+          Test-UnioLabs
         </Link>
       </div>
-      :
-      <>
-      <div className="flex gap-2">
-        <Link to={"/user"}>
-          <button className="btn">User Info</button>
-        </Link>
-      </div>
-      </>
-      }
+      {!user?.email && (
+        <div className="flex gap-2">
+          <Link to={"/register"}>
+            <button className="btn">Register</button>
+          </Link>
+          <Link to={"/login"}>
+            <button className="btn">Login</button>
+          </Link>
+        </div>
+      )}
+      {user?.emailVerified && (
+        <>
+          <div className="flex gap-2">
+            <Link to={"/user"}>
+              <button className="btn">User Info</button>
+            </Link>
+          </div>
+        </>
+      )}
       {user?.emailVerified && (
         <div className="flex gap-2">
           <button className="btn btn-primary">{user?.displayName}</button>
-          <button className="btn btn-warning" onClick={handleLogout}>Logout</button>
+          <button className="btn btn-warning" onClick={handleLogout}>
+            Logout
+          </button>
         </div>
       )}
     </div>
